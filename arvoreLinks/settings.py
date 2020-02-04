0eeea8d7afa646e811090bd2baa3a0ea88843144
rafael <rafaelxld@hotmail.com>
2020-02-04 13:55:03 -0300

    add log

--- arvoreLinks/settings.py
+++ arvoreLinks/settings.py
@@ -37,6 +37,9 @@ USER_AGENT = [
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 CONCURRENT_REQUESTS = 16
+LOG_LEVEL = 'ERROR'
+LOG_FORMAT = '%(levelname)s: %(message)s'
+LOG_FILE = 'logArvore.txt'
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
